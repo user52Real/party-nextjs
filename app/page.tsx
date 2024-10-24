3dbb5bb09fbf6275e9a859bdf9378b4880a75e62
user52 <145881992+user52Real@users.noreply.github.com>
2024-10-24 12:21:07 +0000

    first commit 7

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 
-
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="text-2xl font-bold">
-        Home page
-      </div>
+      <div className="text-2xl">Home Page</div>
     </div>
   );
 }
