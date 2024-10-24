179f9c9ddda5f52205b9ae073828aa2c920d1d6b
user52 <145881992+user52Real@users.noreply.github.com>
2024-10-24 11:59:59 +0000

    first commit 5

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -1,7 +1,7 @@
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function GET(_req: Request) {
+export async function GET() {
     const {userId} = await auth();
     const user = await currentUser();
 
